d16748b85713cc5b91d1afab506a781e4160be75
Manojkumar886 <maddymano434@gmail.com>
2023-04-01 13:04:31 +0530

    Initial Commit and variable and Typecasting is done

--- src/main/java/Fundamental/DemoTypeCasting.java
+++ src/main/java/Fundamental/DemoTypeCasting.java
@@ -1,2 +1,27 @@
-package Fundamental;public class DemoTypeCasting {
+package Fundamental;
+
+public class DemoTypeCasting
+{
+    public static void main(String[] args)
+    {
+//        Widening-Smaller to larger performance
+
+        char myvalue='a';
+        int ALetterAsciValue=myvalue;
+        System.out.println("My value is :"+myvalue+" my value is  A -ASCI NO "+ALetterAsciValue);
+//        Narrowing-Larger to smallest
+
+        double myCGPA=8.1;
+        short CGPASHORTVALUE=(short) myCGPA;
+
+        System.out.println("my college CGPA is "+CGPASHORTVALUE);
+
+//        int a=10,b=20;
+//        float c=8.72f;
+//
+//        int d=(int)(c+a+b);
+//        System.out.println(d);
+
+
+    }
 }
--- src/main/java/Fundamental/SamplePrint.java
+++ src/main/java/Fundamental/SamplePrint.java
@@ -1,2 +1,22 @@
-package Fundamental;public class SamplePrint {
+package Fundamental;
+
+public class SamplePrint
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Core Java");
+
+        String MobileBrand="Realme1";
+        int MobileRAM=8;
+        double Price=24000;
+        char ChargerType='C';
+        Long IMBNO=880099765455564L;
+        float Discount=5.4f;
+
+        System.out.println("My Mobile Brand Name :"+MobileBrand);
+
+        System.out.println("MY Mobile IMBNumber is :"+IMBNO+" \n my Discount :"+Discount+"\t my mobile charge type is -"+ChargerType);
+        System.out.println(Price);
+    }
 }
